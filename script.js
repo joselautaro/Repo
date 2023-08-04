1da0e890d5a05ec3d002c121fb838f0b7bf5e68b
Lautaro Montaña <joselautarom@gmail.com>
2023-08-03 21:53:16 -0300

    Agregamos espacio de reservado mediante almacenamiento en memoria

--- script.js
+++ script.js
@@ -2,4 +2,6 @@ let nombre = prompt("Cual es su nombre?")
 
 let apellido = prompt("Cual es su apellido?")
 
-document.write("Te damos la bienvenida" + "" + nombre + "" + apellido)
+let espacio = " "
+
+document.write("Te damos la bienvenida" + espacio + nombre + espacio + apellido)
